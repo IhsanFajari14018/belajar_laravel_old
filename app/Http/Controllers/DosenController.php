0237d5fdcf7e5e538a1891501963f758e0185a3c
Ihsan Fajari <ihsan.fajari@gmail.com>
2019-08-08 11:54:22 +0700

    Done Tutorial 4

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class DosenController extends Controller{
 
   public function index(){
-    return "Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com";
+
+    $nama = "Ihsan Fajari";
+    $arrMatkul = ["ASD","DAA","ManPro","PBW"];
+    return view('biodata',['nama'=>$nama],['matkul'=>$arrMatkul]);
+    //return "Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com";
   }
 
 }
